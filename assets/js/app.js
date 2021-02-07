d0459d2a7e732b56b350dc4e4533eb1c00ce835a
Colin Densem <colin@summit360.co.uk>
2021-02-07 13:26:21 +0000

    refactor: rename app.scss to .css extension

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,10 @@
 // We need to import the CSS so that webpack will load it.
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
-import "../css/app.scss";
+import '../css/app.css';
 
 // Alpine JS Support
-import Alpine from "alpinejs";
+import Alpine from 'alpinejs';
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
@@ -15,15 +15,15 @@ import Alpine from "alpinejs";
 //     import {Socket} from "phoenix"
 //     import socket from "./socket"
 //
-import "phoenix_html";
-import { Socket } from "phoenix";
-import NProgress from "nprogress";
-import { LiveSocket } from "phoenix_live_view";
+import 'phoenix_html';
+import { Socket } from 'phoenix';
+import NProgress from 'nprogress';
+import { LiveSocket } from 'phoenix_live_view';
 
 let csrfToken = document
   .querySelector("meta[name='csrf-token']")
-  .getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, {
+  .getAttribute('content');
+let liveSocket = new LiveSocket('/live', Socket, {
   params: { _csrf_token: csrfToken },
   dom: {
     onBeforeElUpdated(from, to) {
@@ -35,8 +35,8 @@ let liveSocket = new LiveSocket("/live", Socket, {
 });
 
 // Show progress bar on live navigation and form submits
-window.addEventListener("phx:page-loading-start", (info) => NProgress.start());
-window.addEventListener("phx:page-loading-stop", (info) => NProgress.done());
+window.addEventListener('phx:page-loading-start', (info) => NProgress.start());
+window.addEventListener('phx:page-loading-stop', (info) => NProgress.done());
 
 // connect if there are any LiveViews on the page
 liveSocket.connect();
