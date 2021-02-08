bb9e3bb5a4307c72441100c41020c1067577c266
Colin Densem <colin@summit360.co.uk>
2021-02-08 17:58:43 +0000

    ci: add basic workflow for ci tests
    ci: use Github actions config for postgres
    
    ci: postgres install/config tweaking
    
    ci: set host to localhost for GH actions

--- config/test.exs
+++ config/test.exs
@@ -6,11 +6,18 @@ use Mix.Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :petal, Petal.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "petal_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  username: System.get_env("DB_USER") || "postgres",
+  password: System.get_env("DB_PASS") || "password",
+  hostname: System.get_env("DB_HOST") || "localhost",
+  database: "app_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox
+
+# Configure the database for GitHub Actions
+if System.get_env("GITHUB_ACTIONS") do
+  config :app, Petal.Repo,
+    username: "postgres",
+    password: "postgres"
+end
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
